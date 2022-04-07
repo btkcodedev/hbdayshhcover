993770da8e0671d1029ccea60b72d7763d1080b3
Balasubramanian T K <btk.codedev@gmail.com>
2022-04-07 20:51:32 +0530

    Add files via upload

---
+++ build/precache-manifest.25d0540ee4be3c5c9fc36d4affca4199.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944d60e6b58595425db0ac2db1418db5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b45276fbdbb3ed76c870",
+    "url": "/static/css/main.717ad329.chunk.css"
+  },
+  {
+    "revision": "28a1dfedb204edf60f3a",
+    "url": "/static/js/2.1073962d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1073962d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441beb0d5ff9f637e431",
+    "url": "/static/js/3.0cd646e5.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/static/js/3.0cd646e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45276fbdbb3ed76c870",
+    "url": "/static/js/main.8cd45b98.chunk.js"
+  },
+  {
+    "revision": "b744e781821985603fb0",
+    "url": "/static/js/runtime-main.36e5c69d.js"
+  }
+]);
